546c25f00a7eb1a9b7753e6081a095f9d945e58f
tomer barnea <tomerbarnea123@gmail.com>
2022-06-16 23:47:05 +0300

    server detects client and get info

--- server.js
+++ server.js
@@ -1,15 +1,38 @@
 const express = require('express')
 const app = express()
 const port = 3001
-const server = app.listen(port)
-const io = require('socket.io')(server, {
+// const server = app.listen(port)
+const io = require('socket.io')(port, {
     cors: {
         origin: '*'
     }
 })
 
+let players = {}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-io.on('connection', socket => console.log(socket.id + 'is connected'))
+io.on('connection', connected)
+
+function connected(socket){
+  socket.emit('serverToClient' , '')
+  socket.on('newPlayer', (data) => {
+    console.log('id number: ' + socket.id + 'is connected')
+    players[socket.id] = data
+    console.log('current players : ' + Object.keys(players).length);
+    for(let id in players){
+      console.log(`${players[id].x} is x -- ${players[id].y} is y`);
+    }
+  })
+    socket.on('disconnect', () => {
+      delete players[socket.id]
+      console.log('Goodbye id :' + socket.id +", has disconnected");
+      console.log('current players : ' + Object.keys(players).length);
+      for(let id in players){
+        console.log(`${players[id].x} is x -- ${players[id].y} is y`);
+      }
+    })
+    // socket.on('update', data => console.log(`${data[0].position.x} -- ${data[0].position.y}`))
+}
